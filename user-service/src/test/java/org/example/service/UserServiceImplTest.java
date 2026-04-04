2f44b6ea7864b8e46a96610b9b397fefa40d9df1
LLIASOUBOTINE <semenuk1991311@gmail.com>
2026-04-04 15:11:28 +0200

    fixed error into the name pakage user-service

---
+++ user-service/src/test/java/org/example/service/UserServiceImplTest.java
@@ -0,0 +1,28 @@
+package org.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
